a3aada40e611245b17f1585b2f998da07c8fb3fd
Nacix <anorexicanalt@gmail.com>
2023-02-28 04:24:31 -0500

    Spelling

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 set RUST_BACKTRACE=full
 rem set RUST_BACKTRACE=1
 (hemtt.exe build) && (
-	Echo Addons successfully build!
+	Echo Addons successfully built!
 ) || (
 	Echo Addons failed to build!
 	pause
