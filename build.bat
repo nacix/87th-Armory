b4a3387765b592d3c0d3856ef9311a11d17a756e
Nacix <anorexicanalt@gmail.com>
2023-03-11 07:33:11 -0500

    build: update hemtt commands

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 set RUST_BACKTRACE=full
 rem set RUST_BACKTRACE=1
-(hemtt.exe build) && (
+(hemtt.exe build -vv) && (
 	Echo Addons successfully built!
 ) || (
 	Echo Addons failed to build!
--- dev.bat
+++ dev.bat
@@ -1,7 +1,7 @@
 @echo off
 set RUST_BACKTRACE=full
 rem set RUST_BACKTRACE=1
-(hemtt.exe dev) && (
+(hemtt.exe dev -vv) && (
 	Echo Addons successfully built!
 ) || (
 	Echo Addons failed to build!
