1b713641ef52e115c367adeaa84e4fa01a61c709
Nacix <anorexicanalt@gmail.com>
2023-04-21 16:57:06 -0400

    feat: add arsenal extended support for arf gear
    Added support for Ace Arsenal Extended to prevent arsenal clutter
    
    Finalized build for new ARF gear

---
+++ addons/rep_armor/XtdGearInfos.hpp
@@ -0,0 +1,219 @@
+class XtdGearInfos
+{
+	class CfgWeapons
+	{
+		// Basic ARF Helmet Variants
+		class 87th_ARF_Helmet
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "None";
+			camo = "None";
+		};
+		class 87th_ARF_Helmet_Desert
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "None";
+			camo = "Desert";
+		};
+		class 87th_ARF_Helmet_Sarid
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "None";
+			camo = "Sarid";
+		};
+		class 87th_ARF_Helmet_Woodland
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "None";
+			camo = "Woodland";
+		};
+		class 87th_ARF_Helmet_Winter
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "None";
+			camo = "Winter";
+		};
+		class 87th_ARF_Helmet_Urban
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "None";
+			camo = "Urban";
+		};
+
+		// Mandibled ARF Helmet Variants
+		class 87th_ARF_Helmet_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "Attached";
+			camo = "None";
+		};
+		class 87th_ARF_Helmet_Desert_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "Attached";
+			camo = "Desert";
+		};
+		class 87th_ARF_Helmet_Sarid_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "Attached";
+			camo = "Sarid";
+		};
+		class 87th_ARF_Helmet_Woodland_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "Attached";
+			camo = "Woodland";
+		};
+		class 87th_ARF_Helmet_Winter_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "Attached";
+			camo = "Winter";
+		};
+		class 87th_ARF_Helmet_Urban_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Trooper";
+			mandible = "Attached";
+			camo = "Urban";
+		};
+
+		// Basic ARF Helmet Variants (Tasty)
+		class 87th_ARF_Helmet_Tasty
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "None";
+			camo = "None";
+		};
+		class 87th_ARF_Helmet_Tasty_Desert
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "None";
+			camo = "Desert";
+		};
+		class 87th_ARF_Helmet_Tasty_Sarid
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "None";
+			camo = "Sarid";
+		};
+		class 87th_ARF_Helmet_Tasty_Woodland
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "None";
+			camo = "Woodland";
+		};
+		class 87th_ARF_Helmet_Tasty_Winter
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "None";
+			camo = "Winter";
+		};
+		class 87th_ARF_Helmet_Tasty_Urban
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "None";
+			camo = "Urban";
+		};
+
+		// Mandibled ARF Helmet Variants (Tasty)
+		class 87th_ARF_Helmet_Tasty_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "Attached";
+			camo = "None";
+		};
+		class 87th_ARF_Helmet_Tasty_Desert_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "Attached";
+			camo = "Desert";
+		};
+		class 87th_ARF_Helmet_Tasty_Sarid_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "Attached";
+			camo = "Sarid";
+		};
+		class 87th_ARF_Helmet_Tasty_Woodland_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "Attached";
+			camo = "Woodland";
+		};
+		class 87th_ARF_Helmet_Tasty_Winter_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "Attached";
+			camo = "Winter";
+		};
+		class 87th_ARF_Helmet_Tasty_Urban_P2
+		{
+			model = "87th_ARF_Helmet";
+			variant = "Tasty";
+			mandible = "Attached";
+			camo = "Urban";
+		};
+
+		// ARF Uniform Variants
+		class 87th_ARF_Uniform
+		{
+			model = "87th_ARF_Uniform";
+			variant = "Trooper";
+			camo = "None";
+		};
+		class 87th_ARF_Uniform_Desert
+		{
+			model = "87th_ARF_Uniform";
+			variant = "Trooper";
+			camo = "Desert";
+		};
+		class 87th_ARF_Uniform_Sarid
+		{
+			model = "87th_ARF_Uniform";
+			variant = "Trooper";
+			camo = "Sarid";
+		};
+		class 87th_ARF_Uniform_Woodland
+		{
+			model = "87th_ARF_Uniform";
+			variant = "Trooper";
+			camo = "Woodland";
+		};
+		class 87th_ARF_Uniform_Winter
+		{
+			model = "87th_ARF_Uniform";
+			variant = "Trooper";
+			camo = "Winter";
+		};
+		class 87th_ARF_Uniform_Urban
+		{
+			model = "87th_ARF_Uniform";
+			variant = "Trooper";
+			camo = "Urban";
+		};
+	};
+};
---
+++ addons/rep_armor/XtdGearModels.hpp
@@ -0,0 +1,59 @@
+class XtdGearModels
+{
+	class CfgWeapons 
+	{
+		class 87th_ARF_Helmet
+		{
+			label = "[87th] ARF Helmet";
+			author = "Anorexican";
+			options[] = {"variant", "mandible", "camo"};
+			class variant
+			{
+				label = "Variant";
+				values[] = {"Trooper", "Tasty"};
+				alwaysSelectable = 1;
+			};
+			class mandible
+			{
+				label = "Mandible";
+				values[] = {"None", "Attached"};
+				changeInGame = 1;
+				alwaysSelectable = 1;
+				changeDelay = 5;
+				class None
+				{
+					actionLabel = "Detach Mandible";
+				};
+				class Attached
+				{
+					actionLabel = "Attach Mandible";
+				};
+			};
+			class camo
+			{
+				label = "Camo";
+				values[] = {"None", "Winter", "Desert", "Sarid", "Woodland", "Urban"};
+				alwaysSelectable = 1;
+			};
+		};
+
+		class 87th_ARF_Uniform
+		{
+			label = "[87th] ARF Armor";
+			author = "Anorexican";
+			options[] = {"variant", "camo"};
+			class variant
+			{
+				label = "Variant";
+				values[] = {"Trooper"};
+				alwaysSelectable = 1;
+			};
+			class camo
+			{
+				label = "Camo";
+				values[] = {"None", "Winter", "Desert", "Sarid", "Woodland", "Urban"};
+				alwaysSelectable = 1;
+			};
+		};
+	};
+};
