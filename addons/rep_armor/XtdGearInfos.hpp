8bc451b0110855b6f924c912cd235a6e5f4bde25
Nacix <anorexicanalt@gmail.com>
2023-05-21 23:34:57 -0400

    feat: integrate new xtd macros
    Added macro calls for every updated helmet because I forgot to earlier

--- addons/rep_armor/XtdGearInfos.hpp
+++ addons/rep_armor/XtdGearInfos.hpp
@@ -8,7 +8,7 @@ class XtdGearInfos
 				model = QUOTE(TAG##_P2_Helmet); \
 				variant = QUOTE(variantName); \
 				custom = QUOTE(customName); \
-		};
+			};
 
 		#define XTD_ARF_HELMET_CAMO(className,variantName,camoType) \
 			class TAG##_ARF_##className##_##camoType \
@@ -24,7 +24,7 @@ class XtdGearInfos
 				variant = QUOTE(variantName); \
 				mandible = "Attached"; \
 				camo = QUOTE(camoType); \
-		};
+			};
 		#define XTD_ARF_HELMET(variantName,className) \
 			class TAG##_ARF_##className \
 			{ \
@@ -52,7 +52,7 @@ class XtdGearInfos
 				model = QUOTE(TAG##_ARF_Uniform); \
 				variant = QUOTE(variantName); \
 				camo = QUOTE(camoType); \
-		};
+			};
 		#define XTD_ARF_UNIFORM(variantName,className) \
 			class TAG##_ARF_##className \
 			{ \
@@ -72,7 +72,7 @@ class XtdGearInfos
 				model = QUOTE(TAG##_BARC_Helmet); \
 				variant = QUOTE(variantName); \
 				camo = QUOTE(camoType); \
-		};
+			};
 		#define XTD_BARC_HELMET(variantName,className) \
 			class TAG##_BARC_##className \
 			{ \
@@ -86,167 +86,22 @@ class XtdGearInfos
 			XTD_BARC_HELMET_CAMO(className, variantName, Winter) \
 			XTD_BARC_HELMET_CAMO(className, variantName, Urban)
 
-		// Basic ARF Helmet Variants (Tasty)
-		class 87th_ARF_Helmet_Tasty
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "None";
-			camo = "None";
-		};
-		class 87th_ARF_Helmet_Tasty_Desert
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "None";
-			camo = "Desert";
-		};
-		class 87th_ARF_Helmet_Tasty_Sarid
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "None";
-			camo = "Sarid";
-		};
-		class 87th_ARF_Helmet_Tasty_Woodland
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "None";
-			camo = "Woodland";
-		};
-		class 87th_ARF_Helmet_Tasty_Winter
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "None";
-			camo = "Winter";
-		};
-		class 87th_ARF_Helmet_Tasty_Urban
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "None";
-			camo = "Urban";
-		};
+		XTD_P2_HELMET(Trooper, Trooper, None)
+		XTD_P2_HELMET(Jet_Trooper, Jet, None)
+		XTD_P2_HELMET(ARC, ARC, None)
 
-		// Mandibled ARF Helmet Variants (Tasty)
-		class 87th_ARF_Helmet_Tasty_P2
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "Attached";
-			camo = "None";
-		};
-		class 87th_ARF_Helmet_Tasty_Desert_P2
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "Attached";
-			camo = "Desert";
-		};
-		class 87th_ARF_Helmet_Tasty_Sarid_P2
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "Attached";
-			camo = "Sarid";
-		};
-		class 87th_ARF_Helmet_Tasty_Woodland_P2
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "Attached";
-			camo = "Woodland";
-		};
-		class 87th_ARF_Helmet_Tasty_Winter_P2
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "Attached";
-			camo = "Winter";
-		};
-		class 87th_ARF_Helmet_Tasty_Urban_P2
-		{
-			model = "87th_ARF_Helmet";
-			variant = "Tasty";
-			mandible = "Attached";
-			camo = "Urban";
-		};
+		XTD_P2_HELMET(Alcatraz, Trooper, Alcatraz)
+		XTD_P2_HELMET(Cat, Trooper, Cat)
+		XTD_P2_HELMET(Noggy, Trooper, Noggy)
+		XTD_P2_HELMET(Raven, Trooper, Raven)
+		XTD_P2_HELMET(Stim, Trooper, Stim)
+		XTD_P2_HELMET(Stryder, ARC, Stryder)
 
-		// ARF Uniform Variants
-		class 87th_ARF_Uniform
-		{
-			model = "87th_ARF_Uniform";
-			variant = "Trooper";
-			camo = "None";
-		};
-		class 87th_ARF_Uniform_Desert
-		{
-			model = "87th_ARF_Uniform";
-			variant = "Trooper";
-			camo = "Desert";
-		};
-		class 87th_ARF_Uniform_Sarid
-		{
-			model = "87th_ARF_Uniform";
-			variant = "Trooper";
-			camo = "Sarid";
-		};
-		class 87th_ARF_Uniform_Woodland
-		{
-			model = "87th_ARF_Uniform";
-			variant = "Trooper";
-			camo = "Woodland";
-		};
-		class 87th_ARF_Uniform_Winter
-		{
-			model = "87th_ARF_Uniform";
-			variant = "Trooper";
-			camo = "Winter";
-		};
-		class 87th_ARF_Uniform_Urban
-		{
-			model = "87th_ARF_Uniform";
-			variant = "Trooper";
-			camo = "Urban";
-		};
+		XTD_ARF_HELMET(Trooper, Helmet)
+		XTD_ARF_HELMET(Tasty, Helmet_Tasty)
 
-		class 87th_BARC_Helmet
-		{
-			model = "87th_BARC_Helmet";
-			variant = "Trooper";
-			camo = "None";
-		};
-		class 87th_BARC_Helmet_Desert
-		{
-			model = "87th_BARC_Helmet";
-			variant = "Trooper";
-			camo = "Desert";
-		};
-		class 87th_BARC_Helmet_Sarid
-		{
-			model = "87th_BARC_Helmet";
-			variant = "Trooper";
-			camo = "Sarid";
-		};
-		class 87th_BARC_Helmet_Woodland
-		{
-			model = "87th_BARC_Helmet";
-			variant = "Trooper";
-			camo = "Woodland";
-		};
-		class 87th_BARC_Helmet_Winter
-		{
-			model = "87th_BARC_Helmet";
-			variant = "Trooper";
-			camo = "Winter";
-		};
-		class 87th_BARC_Helmet_Urban
-		{
-			model = "87th_BARC_Helmet";
-			variant = "Trooper";
-			camo = "Urban";
-		};
+		XTD_ARF_UNIFORM(Trooper, Uniform)
+
+		XTD_BARC_HELMET(Trooper, Helmet)
 	};
 };
