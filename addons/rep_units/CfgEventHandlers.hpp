4968bf7f768ae102bde787e06d471e486b40bdb0
Nacix <anorexicanalt@gmail.com>
2023-05-22 17:15:10 -0400

    refactor: use macros for class identity assignment
    Simplified the definitions of class identity assignments using a macro

--- addons/rep_units/CfgEventHandlers.hpp
+++ addons/rep_units/CfgEventHandlers.hpp
@@ -16,158 +16,65 @@ class Extended_PostInit_EventHandlers {
     };
 };
 
+#define SET_RANK(className,grade,rank) \
+	class className { \
+		initServer = QUOTE([ARR_3(_this select 0, '##grade##', '##rank##')] call FUNC(initUnit)); \
+	};
+
 class Extended_InitPost_EventHandlers {
-	class JLTS_Clone_P2_DC15A {
-		initServer = QUOTE([ARR_2(_this select 0, 'CT')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_lieutenant {
-		initServer = QUOTE([ARR_2(_this select 0, 'JCO')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_corporal {
-		initServer = QUOTE([ARR_2(_this select 0, 'NCO')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_AT {
-		initServer = QUOTE([ARR_2(_this select 0, 'SPC')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_marksman {
-		initServer = QUOTE([ARR_2(_this select 0, 'SPC')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_medic {
-		initServer = QUOTE([ARR_2(_this select 0, 'SPC')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_EOD {
-		initServer = QUOTE([ARR_2(_this select 0, 'SPC')] call FUNC(initUnit));
-	};
-	class JLTS_Clone_P2_AR {
-		initServer = QUOTE([ARR_2(_this select 0, 'SPC')] call FUNC(initUnit));
-	};
+	SET_RANK(JLTS_Clone_P2_DC15A, CT, CT)
+	SET_RANK(JLTS_Clone_P2_lieutenant, JCO, CLT)
+	SET_RANK(JLTS_Clone_P2_corporal, NCO, CP)
+	SET_RANK(JLTS_Clone_P2_AT, SPC, CT)
+	SET_RANK(JLTS_Clone_P2_marksman, SPC, CT)
+	SET_RANK(JLTS_Clone_P2_medic, SPC, CT)
+	SET_RANK(JLTS_Clone_P2_EOD, SPC, CT)
+	SET_RANK(JLTS_Clone_P2_AR, SPC, CT)
 
-	class 87th_Clone_Captain {
-		initServer = QUOTE([ARR_3(_this select 0, 'SCO', 'CPT')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Commander {
-		initServer = QUOTE([ARR_3(_this select 0, 'CMDR', 'CC')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Corporal {
-		initServer = QUOTE([ARR_3(_this select 0, 'NCO', 'CP')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Jumper {
-		initServer = QUOTE([ARR_2(_this select 0, 'SPC')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Lieutenant {
-		initServer = QUOTE([ARR_3(_this select 0, 'JCO', 'CLT')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Major {
-		initServer = QUOTE([ARR_3(_this select 0, 'SCO', 'CMJ')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Sergeant {
-		initServer = QUOTE([ARR_3(_this select 0, 'NCO', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_Clone_SergeantMajor {
-		initServer = QUOTE([ARR_3(_this select 0, 'SNCO', 'CSM')] call FUNC(initUnit));
-	};
+	SET_RANK(87th_Clone_Captain, SCO, CPT)
+	SET_RANK(87th_Clone_Commander, CMDR, CC)
+	SET_RANK(87th_Clone_Corporal, NCO, CP)
+	SET_RANK(87th_Clone_Jumper, SPC, CT)
+	SET_RANK(87th_Clone_Lieutenant, JCO, CLT)
+	SET_RANK(87th_Clone_Major, SCO, CMJ)
+	SET_RANK(87th_Clone_Sergeant, NCO, CS)
+	SET_RANK(87th_Clone_SergeantMajor, SNCO, CSM)
 
-	class 87th_Clone_P1_Captain {
-		initServer = QUOTE([ARR_3(_this select 0, 'CT', 'CPT')] call FUNC(initUnit));
-	};
-	class 87th_Clone_P1_Commander {
-		initServer = QUOTE([ARR_3(_this select 0, 'CT', 'CC')] call FUNC(initUnit));
-	};
-	class 87th_Clone_P1_Corporal {
-		initServer = QUOTE([ARR_3(_this select 0, 'CT', 'CP')] call FUNC(initUnit));
-	};
-	class 87th_Clone_P1_Lieutenant {
-		initServer = QUOTE([ARR_3(_this select 0, 'CT', 'CLT')] call FUNC(initUnit));
-	};
-	class 87th_Clone_P1_Sergeant {
-		initServer = QUOTE([ARR_3(_this select 0, 'CT', 'CS')] call FUNC(initUnit));
-	};
+	SET_RANK(87th_Clone_P1_Captain, SCO, CPT)
+	SET_RANK(87th_Clone_P1_Commander, CMDR, CC)
+	SET_RANK(87th_Clone_P1_Corporal, NCO, CP)
+	SET_RANK(87th_Clone_P1_Lieutenant, JCO, CLT)
 
-	class 87th_ARF_DC15S {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'VCT')] call FUNC(initUnit));
-	};
-	class 87th_ARF_Sniper {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'VCT')] call FUNC(initUnit));
-	};
-	class 87th_ARF_Spotter {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CP')] call FUNC(initUnit));
-	};
-	class 87th_ARF_JTAC {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'VCT')] call FUNC(initUnit));
-	};
-	class 87th_ARF_AT {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'VCT')] call FUNC(initUnit));
-	};
-	class 87th_ARF_CLS {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'VCT')] call FUNC(initUnit));
-	};
-	class 87th_ARF_Major {
-		initServer = QUOTE([ARR_3(_this select 0, 'CMDR', 'CMJ')] call FUNC(initUnit));
-	};
+	SET_RANK(87th_ARF_DC15S, SPC, VCT)
+	SET_RANK(87th_ARF_Sniper, SPC, VCT)
+	SET_RANK(87th_ARF_Spotter, SPC, VCT)
+	SET_RANK(87th_ARF_JTAC, SPC, VCT)
+	SET_RANK(87th_ARF_AT, SPC, VCT)
+	SET_RANK(87th_ARF_CLS, SPC, VCT)
+	SET_RANK(87th_ARF_Major, CMDR, CMJ)
 
-	class 87th_AB_DC15C {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'SCT')] call FUNC(initUnit));
-	};
-	class 87th_AB_DC15LE {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'SCT')] call FUNC(initUnit));
-	};
-	class 87th_AB_DC15LE_UGL {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'SCT')] call FUNC(initUnit));
-	};
-	class 87th_AB_AT {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'SCT')] call FUNC(initUnit));
-	};
-	class 87th_AB_AR {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'SCT')] call FUNC(initUnit));
-	};
-	class 87th_AB_CLS {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'SCT')] call FUNC(initUnit));
-	};
+	SET_RANK(87th_AB_DC15C, SPC, SCT)
+	SET_RANK(87th_AB_DC15LE, SPC, SCT)
+	SET_RANK(87th_AB_DC15LE_UGL, SPC, SCT)
+	SET_RANK(87th_AB_AT, SPC, SCT)
+	SET_RANK(87th_AB_AR, SPC, SCT)
+	SET_RANK(87th_AB_CLS, SPC, SCT)
 
-	class 87th_ARC_DC15S {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_Westar {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_GL {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_AT {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_EOD {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_Medic {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_JTAC {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_Sniper {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'CS')] call FUNC(initUnit));
-	};
-	class 87th_ARC_Lieutenant {
-		initServer = QUOTE([ARR_3(_this select 0, 'SCO', 'CLT')] call FUNC(initUnit));
-	};
+	SET_RANK(87th_ARC_DC15S, SPC, CS)
+	SET_RANK(87th_ARC_Westar, SPC, CS)
+	SET_RANK(87th_ARC_GL, SPC, CS)
+	SET_RANK(87th_ARC_AT, SPC, CS)
+	SET_RANK(87th_ARC_EOD, SPC, CS)
+	SET_RANK(87th_ARC_Medic, SPC, CS)
+	SET_RANK(87th_ARC_JTAC, SPC, CS)
+	SET_RANK(87th_ARC_Sniper, SPC, CS)
+	SET_RANK(87th_ARC_Lieutenant, SCO, CLT)
 
-	class 87th_RC_DC17M {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'RC')] call FUNC(initUnit));
-	};
-	class 87th_RC_NCO {
-		initServer = QUOTE([ARR_3(_this select 0, 'NCO', 'RC')] call FUNC(initUnit));
-	};
-	class 87th_RC_Tech {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'RC')] call FUNC(initUnit));
-	};
-	class 87th_RC_Demo {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'RC')] call FUNC(initUnit));
-	};
-	class 87th_RC_Sniper {
-		initServer = QUOTE([ARR_3(_this select 0, 'SPC', 'RC')] call FUNC(initUnit));
-	};
-	class 87th_Clone_Pilot {
-		initServer = QUOTE([ARR_3(_this select 0, 'CT', 'CX')] call FUNC(initUnit)); 
-	};
+	SET_RANK(87th_RC_DC17M, SPC, RC)
+	SET_RANK(87th_RC_NCO, NCO, RC)
+	SET_RANK(87th_RC_Tech, SPC, RC)
+	SET_RANK(87th_RC_Demo, SPC, RC)
+	SET_RANK(87th_RC_Sniper, SPC, RC)
+
+	SET_RANK(87th_Clone_Pilot, CT, CX)
 };
