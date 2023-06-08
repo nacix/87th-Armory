11c10c042c5d2646c1c7874eb3c25e7690d8d8c6
Nacix <anorexicanalt@gmail.com>
2023-06-07 22:03:33 -0400

    feat: update mod/faction logos
    Added CIS faction logo
    
    Update small mod logo

--- addons/cis_units/CfgFactionClasses.hpp
+++ addons/cis_units/CfgFactionClasses.hpp
@@ -3,11 +3,11 @@ class CfgFactionClasses
 	NEW_CLASS(CIS)
 	{
 		displayName = TAG_NAME(Separatist Alliance);
-		icon = QPATHTOF(data\ui\TAG_CLASS(logo_faction).paa);
+		icon = QPATHTOF(data\ui\logo_cis.paa);
 	};
     NEW_CLASS(CIS_Militia)
 	{
 		displayName = TAG_NAME(Planetary Militia);
-		icon = QPATHTOF(data\ui\TAG_CLASS(logo_faction).paa);
+		icon = QPATHTOF(data\ui\logo_cis.paa);
 	};
 };
