558cc3d524a28f22364c4de09833befb55b4d6f2
Nacix <anorexicanalt@gmail.com>
2023-06-09 15:22:25 -0400

    refactor: change droid faction displayname

--- addons/cis_units/CfgFactionClasses.hpp
+++ addons/cis_units/CfgFactionClasses.hpp
@@ -2,7 +2,7 @@ class CfgFactionClasses
 {
 	NEW_CLASS(CIS)
 	{
-		displayName = TAG_NAME(Separatist Alliance);
+		displayName = TAG_NAME(CIS Droid Army);
 		icon = QPATHTOF(data\ui\logo_cis.paa);
 	};
     NEW_CLASS(CIS_Militia)
