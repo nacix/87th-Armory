9de558db835d5450b5f98bd99017fd950dee603d
Nacix <anorexicanalt@gmail.com>
2023-06-09 16:07:09 -0400

    fix: hide vibroblade on bx captain
    Set BX Captain's weapon scale to 0
    
    Resolves issue where BX Captain's melee weapon floated off their hip

--- addons/cis_weapons/CfgWeapons.hpp
+++ addons/cis_weapons/CfgWeapons.hpp
@@ -1,6 +1,11 @@
 class CfgWeapons
 {
     class SWLW_Z7;
+    class WBK_lightsaber_sith;
+    class WBK_Dutch_Vibro: WBK_lightsaber_sith
+    {
+        class WeaponSlotsInfo;
+    };
 
     NEW_CLASS(Z7): SWLW_Z7
     {
@@ -13,5 +18,15 @@ class CfgWeapons
         aiRateOfFireDispersion = 0;
         aiRateOfFireDistance = 500;
         autoFire = 1;
+    };
+    NEW_CLASS(Vibroblade): WBK_Dutch_Vibro
+    {
+        scope = 1;
+        author = "Anorexican";
+        class WeaponSlotsInfo: WeaponSlotsInfo
+        {
+            holsterOffset = "";
+            holsterScale = 0;
+        };
     };
 };
